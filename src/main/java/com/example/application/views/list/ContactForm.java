d8ef93474418ebbd48f94ff3f623d877e381711c
arabiki <arabiki330@gmail.com>
2023-08-10 18:56:05 +0900

    フォームにイベントに対するアクションを追加

--- src/main/java/com/example/application/views/list/ContactForm.java
+++ src/main/java/com/example/application/views/list/ContactForm.java
@@ -2,7 +2,10 @@ package com.example.application.views.list;
 
 
 import com.example.application.data.entity.Company;
+import com.example.application.data.entity.Contact;
 import com.example.application.data.entity.Status;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -11,6 +14,9 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.shared.Registration;
 
 import java.util.List;
 
@@ -28,9 +34,13 @@ public class ContactForm extends FormLayout {
     Button delete = new Button("Delete");
     Button close = new Button("Cancel");
 
+    // フォームから取得したデータのバインド先のオブジェクトを定義
+    Binder<Contact> binder = new BeanValidationBinder<>(Contact.class);
+
     public ContactForm(List<Company> companies, List<Status> statuses) {
         // フォームコンポーネントの名前を設定
         addClassName("contact-form");
+        binder.bindInstanceFields(this);
 
         // プルダウンの中身を設定する（Company型でもらって、中身を取り出す？仕様ぽい）
         company.setItems(companies);
@@ -59,8 +69,69 @@ public class ContactForm extends FormLayout {
         save.addClickShortcut(Key.ENTER);
         close.addClickShortcut(Key.ESCAPE);
 
+        save.addClickListener(event -> validateAndSave());
+        delete.addClickListener(event -> fireEvent(new DeleteEvent(this, binder.getBean())));
+        close.addClickListener(event -> fireEvent(new CloseEvent(this)));
+
+        binder.addStatusChangeListener(e -> save.setEnabled(binder.isValid()));
+
+
         // ３津のボタンを横方向に配置する
         return new HorizontalLayout(save, delete, close);
+    }
+
+    public void setContact(Contact contact){
+        binder.setBean(contact);
+    }
+
+    private void validateAndSave(){
+        if (binder.isValid()){
+            fireEvent(new SaveEvent(this, binder.getBean()));
+        }
+    }
+
+
+    //  ContactFormEventはすべてのイベント共通のスーパークラス
+    public static abstract class ContactFormEvent extends ComponentEvent<ContactForm>{
+        private Contact contact;
+
+        protected ContactFormEvent(ContactForm source, Contact contact) {
+            super(source, false);
+            this.contact = contact;
+        }
+
+        public Contact getContact() {
+            return contact;
+        }
+    }
+
+    public static class SaveEvent extends ContactFormEvent {
+        SaveEvent(ContactForm source, Contact contact) {
+            super(source, contact);
+        }
+    }
+
+    public static class DeleteEvent extends ContactFormEvent {
+        DeleteEvent(ContactForm source, Contact contact) {
+            super(source, contact);
+        }
+    }
+
+    public static class CloseEvent extends ContactFormEvent {
+        CloseEvent(ContactForm source) {
+            super(source, null);
+        }
+    }
+
+    public Registration addDeleteListener(ComponentEventListener<DeleteEvent> listener) {
+        return addListener(DeleteEvent.class, listener);
+    }
+
+    public Registration addSaveListener(ComponentEventListener<SaveEvent> listener) {
+        return addListener(SaveEvent.class, listener);
+    }
+    public Registration addCloseListener(ComponentEventListener<CloseEvent> listener) {
+        return addListener(CloseEvent.class, listener);
     }
 
 
